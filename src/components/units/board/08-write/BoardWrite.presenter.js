783e2050438239313a7f496d6570b0be2e8d27eb
IMHEEJAE <imheejae95@gmail.com>
2023-02-28 13:02:30 +0900

    게시글 수정

---
+++ src/components/units/board/08-write/BoardWrite.presenter.js
@@ -0,0 +1,17 @@
+import { BlueButton, RedInput } from "./BoardWrite.styles";
+
+export default function BoardWriteUI(props) {
+  return (
+    <>
+      작성자 : <RedInput type="text" onChange={props.onChangeWriter} />
+      <br />
+      제목 : <input type="text" onChange={props.onChangeTitle} />
+      <br />
+      내용 : <input type="text" onChange={props.onChagneContents} />
+      <br />
+      <BlueButton zzz={props.mycolor} onClick={props.isEdit ? props.onClickUpdate : props.onClickSubmit}>
+        {props.isEdit ? "수정하기" : "등록하기"}
+      </BlueButton>
+    </>
+  );
+}
---
+++ src/components/units/board/08-write/BoardWrite.styles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const RedInput = styled.input`
+  border-color: red;
+`;
+export const BlueButton = styled.button`
+  font-size: ${(props) => props.rrr};
+  background-color: ${(props) => (props.zzz ? "blue" : "yellow")};
+`;
